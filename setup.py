6ea3ddb63b500f426e88450ddde8438a9eed3145
mrMekdad <mekdadr6@gmail.com>
2024-01-05 23:29:00 +0100

    proteomics-normalizer: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2024-proteomics-normalizer",
+    version="0.1.0",
+    description="Normalization and export helpers for proteomics abundance tables.",
+    author="Red@",
+    packages=["proteomics_normalizer"],
+    package_dir={"": "src"},
+)
